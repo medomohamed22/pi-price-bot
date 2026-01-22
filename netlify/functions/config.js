912770b3f36e113ad9e3e4cde1f11b77ea196755
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-01-22 13:19:40 +0200

    Create config.js

---
+++ netlify/functions/config.js
@@ -0,0 +1,13 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store"
+    },
+    body: JSON.stringify({
+      SB_URL: process.env.PUBLIC_SUPABASE_URL,
+      SB_ANON: process.env.PUBLIC_SUPABASE_ANON_KEY
+    })
+  };
+};
