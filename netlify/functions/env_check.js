58d7aff88a5a1ba4431b3e2955873a1c6661f424
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-01-29 23:47:15 +0200

    Delete netlify/functions/env_check.js

--- netlify/functions/env_check.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async () => {
-  const keys = ["ISSUER_SECRET", "DISTRIBUTOR_SECRET", "PI_API_KEY", "ADMIN_TOKEN"];
-  const status = {};
-  for (const k of keys) status[k] = process.env[k] ? "SET" : "MISSING";
-  return { statusCode: 200, body: JSON.stringify({ status }, null, 2) };
-};
